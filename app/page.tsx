6274b8381802022fba6f3b62cc837c7946d1fb64
s92i <sofiane.92izii@gmail.com>
2022-12-10 22:51:17 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import { categories } from '../constants'
+import fetchNews from '../lib/fetchNews'
+import NewsList from './NewsList'
+//import response from '../response.json'
+
+async function Homepage() {
+    const news: NewsResponse = await fetchNews(categories.join(','))
+
+    return (
+        <div>
+            <NewsList news={news} />
+        </div>
+    )
+}
+
+export default Homepage
